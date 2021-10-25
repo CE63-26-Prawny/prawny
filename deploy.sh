ac1f394204ece3078303c973af0155985ed65dc7
N8NNY <60010421@kmitl.ac.th>
2021-10-25 22:48:26 +0700

    :sparkles: add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-echo "$(tput setab 3)-------Copy .env file to .env-list---------$(tput sgr0)"
-echo "$(sed 's/export //g' .env > .env-list)"
 echo "$(tput setab 3)-------Build image-------------------------$(tput sgr0)"
 docker-compose build
 echo "$(tput setab 1)-------Down container----------------------$(tput sgr0)"
 docker-compose down 
 echo "$(tput setab 2)-------Docker-compose up-------------------$(tput sgr0)"
 docker-compose up -d
-echo "$(tput setab 2)-------Restart KTB-API-------------------$(tput sgr0)"
-sleep 8
-docker restart ktbacsbackend_ktb-api_1
 if [ $? -eq 0 ] 
 then
     echo "$(tput setab 2)-------DEPLOY COMPLETE---------------------$(tput sgr0)"
